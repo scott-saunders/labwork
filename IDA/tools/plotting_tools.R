725c8320342ad47b01437fd161dca8dcf6671cc6
Scott Saunders <scottsaunders@Scotts-MacBook-Pro-2.local>
2019-02-17 23:17:55 -0800

    updated figures and derivation

--- IDA/tools/plotting_tools.R
+++ IDA/tools/plotting_tools.R
@@ -1,15 +1,33 @@
 library(ggplot2)
+library(extrafont)
+
+#base_family = "Calibri"
 
 theme_1 <- function () { 
-  theme_classic() %+replace% 
+  theme_classic( ) %+replace% 
     theme(
-      axis.text = element_text( size=12),
-      axis.title=element_text(size=14),
-      strip.text = element_text(size = 14),
-      strip.background = element_rect(color='white'),
-      legend.title=element_text(size=14),
-      legend.text=element_text(size=12),
-      legend.text.align=0,
-      panel.grid.major = element_line(color='grey',size=0.1)
+      axis.text = element_text( size=10),
+      axis.title=element_text(size=12),
+      strip.text = element_text(size = 12),
+      strip.background = element_blank(),
+      legend.background = element_blank(),
+      legend.title=element_text(size=12),
+      legend.text=element_text(size=10),
+      legend.text.align=0
     )
+}
+
+nA_label <- function(x){
+# from A to nA
+  lab <- x / 1e-9
+}
+
+mV_label <- function(x){
+# From V to mV
+  lab <- x * 1000
+}
+
+fold_label <- function(x){
+#e.g. 2 to 2x
+  lab <- paste(x, "x", sep = '')
 }
