36e0b00788bcbf3d27bb15afa0766b9b994e6d08
scott-saunders <ssaunder@caltech.edu>
2019-10-01 10:17:00 -0700

    pres_test, random

--- IDA/tools/readme_generator.R
+++ IDA/tools/readme_generator.R
@@ -1,19 +1,22 @@
 library(rmarkdown)
 
-# Grab all README files
-files <- dir(path = '.', pattern = 'README',include.dirs = F,recursive = T, ignore.case = T)
-
-# Just take the actual .rmd files
-readme <- grep(pattern = "rmd", x = files, value = T, ignore.case = T)
-
-# Add the index file
-to_knit <- c(readme,'index.Rmd')
-
-# loop through and render github version then html version with parameters in yaml
-for(f in to_knit) {
-  render(f, 'github_document')
-  render(f, 'html_document')
+knit_readme_all <- function(){
+  # Grab all README files
+  files <- dir(path = '.', pattern = 'README',include.dirs = F,recursive = T, ignore.case = T)
+  print(files)
+  # Just take the actual .rmd files
+  #readme <- grep(pattern = "rmd", x = files, value = T, ignore.case = T)
+  
+  # Add the index file
+ # to_knit <- c(readme,'index.Rmd')
+  
+  # loop through and render github version then html version with parameters in yaml
+  #for(f in to_knit) {
+  #  render(f, 'github_document')
+  #  render(f, 'html_document')
+  #}
 }
+
 
 # Function for updating All READMEs 
 
@@ -23,5 +26,13 @@ for(f in to_knit) {
 
 generate_readme <- function() {
   wd = getwd()
-  file.copy(from = "/labwork/IDA/tools/09_05_19_sandbox.html", to = wd)
+  print(wd)
+  wd = paste(wd, "README.Rmd", sep = '/')
+  print(wd)
+  #file.copy(from = "/labwork/IDA/tools/09_05_19_sandbox.html", to = wd )
+}
+
+knit_readme <- function(path = '.', recursive = T){
+  files <- dir(path = path, pattern = 'README',include.dirs = F,recursive = recursive, ignore.case = T)
+  print(files)
 }
